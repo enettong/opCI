336009db773ce906a06c2a734ee1db3a1c1882b1
enettong <58711679+enettong@users.noreply.github.com>
2020-06-23 23:07:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,17 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+# Add a feeds
+git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+git clone https://github.com/kenzok8/small package/small
+
+#Add a feeds
+git clone https;//github.com/lwz322/luci-app-k3screenctrl package/luci-app-k3screenctrl
+git clone https://github.com/lwz322/k3screenctrl_build package/k3screenctrl_build
+
+
+
+#update & install
+./scripts/feeds update -a
+./scripts/feeds install -a
