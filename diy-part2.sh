6f9c4b828c02684c482f0ce9a941458cbcc414d0
enettong <58711679+enettong@users.noreply.github.com>
2024-02-23 11:21:26 -0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,34 +9,34 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 #enable WiFi
-sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Add a feeds
-git clone https://github.com/coolsnowwolf/lede
-cp -rf ./lede/package/lean ./package/
+#git clone https://github.com/coolsnowwolf/lede
+#cp -rf ./lede/package/lean ./package/
 #cp -rf ./lede/tools/upx ./tools/
 #cp -rf ./lede/tools/ucl ./tools/
 #sed -i '11a tools-y += ucl upx' ./tools/Makefile
 #sed -i '43a $(curdir)/upx/compile := $(curdir)/ucl/compile' ./tools/Makefile
-rm -rf ./lede
-rm -rf ./package/lean/k3screenctrl
-git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-git clone https://github.com/kenzok8/small package/small
+#rm -rf ./lede
+#rm -rf ./package/lean/k3screenctrl
+#git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+#git clone https://github.com/kenzok8/small package/small
 
 #Add a feeds
-git clone https://github.com/lwz322/luci-app-k3screenctrl package/luci-app-k3screenctrl
-git clone https://github.com/lwz322/k3screenctrl_build package/k3screenctrl_build
+#git clone https://github.com/lwz322/luci-app-k3screenctrl package/luci-app-k3screenctrl
+#git clone https://github.com/lwz322/k3screenctrl_build package/k3screenctrl_build
 
 #Add a feeds
-git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR package/luci-app-koolproxyR
+#git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR package/luci-app-koolproxyR
 
 
 #update & install
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
 
 #Fix upx
-sudo ln /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/upx
+#sudo ln /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/upx
