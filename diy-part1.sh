e82bbdfdbebd0775a45940cca06548a649c081c0
enettong <58711679+enettong@users.noreply.github.com>
2024-02-24 02:28:00 -0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,3 +10,6 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+#Add a feeds
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
